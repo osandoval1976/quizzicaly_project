798ed0183ca7926351423d69fc2aacebe6cba7f7
osandoval1976 <oscarhs0576@gmail.com>
2026-07-16 06:07:58 -0400

    updated

--- quizz-trivia/my-app/src/App.js
+++ quizz-trivia/my-app/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Quizz from './Quizz';
 function App() {
+
+function handleClick() {
+  
+  console.log("Button clicked");
+}
+
+
+
   return (
     <div className="App">
-    <h1>Quizzical</h1>
-    <button>Start quiz</button>
+    <h1 className='app-title'>Quizzical</h1>
+    <p className='app-text'>Some description if needed</p>
+    <button className='btn-title' onClick={handleClick}>
+      Start Quiz
+    </button> 
     </div>
   );
 }
---
+++ quizz-trivia/my-app/src/Quizz.js
@@ -0,0 +1,4 @@
+export default function Quizz() {
+
+    console.log("Quizz component rendered");
+}
