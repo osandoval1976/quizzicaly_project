4db792af033d65f161dc38ca80c6f11d6058bf7f
osandoval1976 <oscarhs0576@gmail.com>
2026-07-15 20:16:13 -0400

    updated

---
+++ quizz-trivia/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Quizzical</h1>
+    <button>Start quiz</button>
+    </div>
+  );
+}
+
+export default App;
