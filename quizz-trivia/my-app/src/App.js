8a7fd76373391c5024dad4d2cf6734faf86f15d1
osandoval1976 <oscarhs0576@gmail.com>
2026-07-18 16:00:56 -0400

    updated

--- quizz-trivia/my-app/src/App.js
+++ quizz-trivia/my-app/src/App.js
@@ -1,36 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-import Quizz from './Quizz';
-import {useState,React} from 'react';
+import { useState, useEffect, React } from 'react';
+
 
 export default function App() {
-;
+  const [isLoading, setIsLoading] = useState(true);
+  const [datas, setDatas] = useState([]);
 
-function handleClick() {
-return (
-  <div>
-    <Quizz />
-  </div>
-)
+  const getMovies = async () => {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=4');
+      const data = await response.json();
+      setDatas(data.results || []);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   
-
-
-}
-
+  
   return (
-    
-    <div className="App">
+    <section className='App-section-1'>
       <div>
-    <h1 className='app-title'>Quizzical</h1>
-    <p className='app-text'>Some description if needed</p>
-    <button className='btn-title' onClick={() => handleClick}>
-    Start-Quizing
-   </button>
-    </div>
-    <div>
-      <Quizz />
-    </div>
-    </div>
+        {isLoading ? (
+          'Loading...'
+        ) : (datas.map((data, index) => (
+           
+            <div key={`${data.question}-${index}`}>
+              <h2 className='f-size'>{data.question}</h2>
+              <p className='mode-list'>Difficulty: {data.difficulty}</p>
+              <span className='q-list'>{[...data.correct_answer, data.incorrect_answers]}</span>
+               
+              
+            </div>
+          ))
+        )
+          }
+      </div>
+    </section>
   );
 }
 
--- quizz-trivia/my-app/src/Quizz.js
+++ quizz-trivia/my-app/src/Quizz.js
@@ -1,28 +1,7 @@
-import { useState, useEffect } from 'react';
 
 export default function Quizz() {
-    const [questions, setQuestions] = useState(true);
-    const [data, setData] = useState([]);
-    console.log(questions)
-    const getMovies = async () => {
-  try{
-const rest =   await fetch('https://opentdb.com/api.php?amount=4')
-const data = await rest.json()
-setQuestions(data)
- 
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-  }
-}
-
-useEffect(() => {
-  getMovies()
-
-}
-, [])
-
- return (
-    <h1>{questions.question}</h1>
-  )
+   
+const answers = Math.floor(Math.random() * 100 )
+console.log(answers)
 }
 
