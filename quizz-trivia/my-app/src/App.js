6c9325aeb293d7190fbd7ab86d6ad4a557672dd9
osandoval1976 <oscarhs0576@gmail.com>
2026-07-17 20:44:02 -0400

    updated

--- quizz-trivia/my-app/src/App.js
+++ quizz-trivia/my-app/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Quizz from './Quizz';
-function App() {
+import {useState,React} from 'react';
+
+export default function App() {
+;
 
 function handleClick() {
+return (
+  <div>
+    <Quizz />
+  </div>
+)
   
-  console.log("Button clicked");
+
+
 }
 
-
-
   return (
+    
     <div className="App">
+      <div>
     <h1 className='app-title'>Quizzical</h1>
     <p className='app-text'>Some description if needed</p>
-    <button className='btn-title' onClick={handleClick}>
-      Start Quiz
-    </button> 
+    <button className='btn-title' onClick={() => handleClick}>
+    Start-Quizing
+   </button>
+    </div>
+    <div>
+      <Quizz />
+    </div>
     </div>
   );
 }
 
-export default App;
+
--- quizz-trivia/my-app/src/Quizz.js
+++ quizz-trivia/my-app/src/Quizz.js
@@ -1,4 +1,27 @@
-export default function Quizz() {
+import { useState, useEffect } from 'react';
 
-    console.log("Quizz component rendered");
+export default function Quizz() {
+    const [questions, setQuestions] = useState(true);
+    const [data, setData] = useState([]);
+    console.log(questions)
+    const getMovies = async () => {
+  try{
+const rest =   await fetch('https://opentdb.com/api.php?amount=4')
+const data = await rest.json()
+setQuestions(data)
+ 
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+}
+
+useEffect(() => {
+  getMovies()
+
+}
+, [])
+
+ return (
+    <h1>{questions.questions}</h1>
+  )
 }
