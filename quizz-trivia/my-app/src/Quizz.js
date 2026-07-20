13a5faea7773b45b3094239aa9114b2d90859b7b
Racso <oscarhs0576@gmail.com>
2026-07-20 11:19:49 -0400

    Update Quizz.js

--- quizz-trivia/my-app/src/Quizz.js
+++ quizz-trivia/my-app/src/Quizz.js
@@ -1,49 +1,6 @@
-import {useState, useEffect, React} from 'react
+
 
 export default function Quizz() {
-
-const [isLoading, setIsLoading] = useState(true);
-  const [datas, setDatas] = useState([]);
- /*Fetching the trivia Api*/ 
-const getMovies = async () => {
-    try {
-      const response = await fetch('https://opentdb.com/api.php?amount=4');
-      const data = await response.json();
-      setDatas(data.results || []);
-    } catch (error) {
-      console.error('Error fetching movies:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  
- /*Mapping throught the data of triva Api*/
- const answers =  (datas.map((data, index) =>{
- const dataNumbers = [data.correct_answer +', ' + data.incorrect_answers]
-
-  console.log(data.question)
-
-  function getRandomnumber(array){
-   
-   const randonNumber = Math.floor(Math.random() * array.length)
-   console.log(array[randonNumber])
-   return array[randonNumber]
-
-  }
- 
-  return (
-           
-            <div key={`${data.question}-${index}`}>
-              <h2 className='questions'>{data.question}</h2>
-              <li className='answers' key={index}>{getRandomnumber(dataNumbers)}</li>
-              </div>
-          )}
-        )
-        )
 
 }
 
