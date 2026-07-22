98edfb4d501248423db6a6467055067a4de0bfea
Racso <oscarhs0576@gmail.com>
2026-07-22 14:39:25 -0400

    Update Quizz.js

--- quizz-trivia/my-app/src/Quizz.js
+++ quizz-trivia/my-app/src/Quizz.js
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+const initialItems = ["Item A", "Item B", "Item C", "Item D"];
+export default function Quizz() {
+  const [items, setItems] = useState(initialItems);
+  const [visible, setInvisible] = useState(false)
+  const shuffleArray = () => {
+    // Clone the array first to avoid mutating state directly
+    const shuffled = [...items]; 
+    
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
+    }
+    
+    setItems(shuffled);
+    setInvisible(items)
+  };
+ 
+  return (
+    <div>
+    {!visible? <button onClick={shuffleArray}>Shuffle List</button> :  <ul>
+        {/* WARNING: Never use Math.random() or index as the element key here */}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>}
+      
+    </div>
+  );
+}
 
-
-
-
-export default function Quizz(props) {
-
-const answers =  [props.correct_answer,...props.incorrect_answers]
-
-
-const random = [Math.floor(Math.random() * answers.length)]
-console.log(answers)
-return random
 
    
-}
+
 
