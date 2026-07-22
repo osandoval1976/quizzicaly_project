8a8e50d3f0a568418f4b035f1125391739c43a46
Racso <oscarhs0576@gmail.com>
2026-07-22 14:43:44 -0400

    Remove commented documentation on shuffling algorithm
    Removed commented-out documentation about the Fisher–Yates shuffle algorithm.

--- quizz-trivia/my-app/src/App.js
+++ quizz-trivia/my-app/src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import { useState, useEffect, React } from 'react';
 import Quizz from './Quizz';
@@ -46,5 +47,11 @@ return (
     </section>
   );
 }
-
-
+/*"The Fisher–Yates shuffle is an algorithm for shuffling a finite sequence. 
+The algorithm takes a list of all the elements of the sequence, and continually determines 
+the next element in the shuffled sequence by randomly drawing an element from the list until no elements remain. 
+The algorithm produces an unbiased permutation: every permutation is equally likely. The modern version of the 
+algorithm takes time proportional to the number of items being shuffled and shuffles them in place."
+https://www.youtube.com/watch?v=FGAUekwri1Q
+Modificate Shuffing sequence: https://onecompiler.com/react/44vz6tfy8
+*/
