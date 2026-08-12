cacbf6d2bbee18bfd6e0758bbc49f7d5f89d38ce
Racso <oscarhs0576@gmail.com>
2026-08-12 13:29:03 -0400

    Implement confetti effect on perfect score
    Added confetti effect upon scoring full points and updated answer checking logic.

--- quizz-trivia/my-app/src/App.js
+++ quizz-trivia/my-app/src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import shuffleArray from './Quizz';
 import { clsx } from 'clsx';
 import { decode } from 'html-entities';
+import confetti from 'canvas-confetti';
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [visible, setVisible] = useState(false);
@@ -65,12 +66,21 @@ export default function App() {
   const checkAnswers = () => {
     let finalScore = 0;
     questions.forEach((item, index) => {
-      if (guessAnswer[index] === item.correct_answer) {
+      if (guessAnswer[index] === item.correctAnswerClean) {
         finalScore++;
       }
     });
     setScore(finalScore);
     setIsQuizOver(true);
+
+    // 2. ¡Lanzamos confeti al comprobar las respuestas!
+    if (finalScore === questions.length) {
+      confetti({
+        particleCount: 150,
+        spread: 80,
+        origin: { y: 0.6 }
+      });
+    }
   };
 
   // Función para reiniciar todo el juego por completo
@@ -165,49 +175,3 @@ export default function App() {
     </div>
   );
 }
-
-/*"The Fisher–Yates shuffle is an algorithm for shuffling a finite sequence. 
-The algorithm takes a list of all the elements of the sequence, and continually determines 
-the next element in the shuffled sequence by randomly drawing an element from the list until no elements remain. 
-The algorithm produces an unbiased permutation: every permutation is equally likely. The modern version of the 
-algorithm takes time proportional to the number of items being shuffled and shuffles them in place."
-https://www.youtube.com/watch?v=FGAUekwri1Q
-Modificate Shuffing sequence: https://onecompiler.com/react/44vz6tfy8
-
-
-*/
-
-/*import { useState } from 'react';
-
-const initialItems = ["Item A", "Item B", "Item C", "Item D"];
-
-export default function ShuffleList() {
-  const [items, setItems] = useState(initialItems);
-  const [visible, setInvisible] = useState(false)
-  const shuffleArray = () => {
-    // Clone the array first to avoid mutating state directly
-    const shuffled = [...items]; 
-    
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap
-    }
-    
-    setItems(shuffled);
-    setInvisible(items)
-  };
- 
-  return (
-    <div>
-    {!visible? <button onClick={shuffleArray}>Shuffle List</button> :  <ul>
-        {/* WARNING: Never use Math.random() or index as the element key here *//*}
-        <h1>Answer Quizz Questions</h1>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>}
-      
-    </div>
-  );
-}
-**/
